5cccc16d4476bdfbb5a367b36a02feaea38c31aa
Linh Vo <lhvo2112@gmail.com>
2026-03-17 20:50:53 +0200

    add database schema and migration

---
+++ migrations/1_add_table.down.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS devices;
+
+DROP TABLE IF EXISTS habit_rules;
+
+DROP TABLE IF EXISTS pet_states;
+
+DROP TABLE IF EXISTS activity_events;
+
+DROP extension IF EXISTS timescaledb;
--- server/main.go
+++ server/main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"database/sql"
+	"log"
+	// "net/http"
+	"os"
+
+	// "github.com/gin-gonic/gin"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	router := gin.Default()
+	db_url := os.Getenv("POSTGRES_URL")
 
-	router.GET("/test", func(c *gin.Context) {
-		c.String(http.StatusOK, "test GET message")
-	})
+	db, err := sql.Open("postgres", db_url)
 
-	router.Run(":8080")
+	if err != nil {
+		log.Fatalf("error connecting to database:\n%s", err)
+	}
+
+	// defer func() {
+	// 	if err := db.Close(); err != nil {
+	// 		log.Fatal("error closing database")
+	// 	}
+	// }()
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatalf("can't ping db:\n%s", err)
+	}
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+
+	if err != nil {
+		log.Fatalf("error configuring driver:\n%s", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./migrations",
+		"postgres", driver)
+
+	if err != nil {
+		log.Fatalf("error migrating:\n%s", err)
+	}
+
+	err = m.Up()
+
+	if err != nil {
+		log.Fatalf("error migrating db:\n%s", err)
+	}
+
+	// router := gin.Default()
+	//
+	// router.GET("/test", func(c *gin.Context) {
+	// 	c.String(http.StatusOK, "test GET message")
+	// })
+	//
+	// router.Run(":8080")
 }
