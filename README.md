46b52d68309412f185021f67472526b02f7c231b
Linh Vo <lhvo2112@gmail.com>
2026-03-18 12:26:06 +0200

    move database migration to docker container

--- README.md
+++ README.md
@@ -14,12 +14,16 @@ go mod download
 POSTGRES_DB=coffeebud_db
 POSTGRES_USER=<your_username>
 POSTGRES_PASSWORD=<your_password>
-
+POSTGRES_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@database:5432/${POSTGRES_DB}?sslmode=disable
+APP_POSTGRES_URL=postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DB}?sslmode=disable
 ```
+
 4. Set up database
 ```bash
-docker compose up
+docker compose up -d
 ```
+
+To verify that the database tables are set up correctly, run `docker exec -it coffeebud_database psql coffeebud_db -U <your_username>` and use command `\dt` in `psql` to list all the tables created.
 
 5. Start the server
 ```bash
--- server/main.go
+++ server/main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"database/sql"
 	"log"
-	// "net/http"
+	"net/http"
 	"os"
 
-	// "github.com/gin-gonic/gin"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	db_url := os.Getenv("POSTGRES_URL")
+	/** DATABASE **/
+	db_url := os.Getenv("APP_POSTGRES_URL")
 
 	db, err := sql.Open("postgres", db_url)
 
@@ -23,42 +21,24 @@ func main() {
 		log.Fatalf("error connecting to database:\n%s", err)
 	}
 
-	// defer func() {
-	// 	if err := db.Close(); err != nil {
-	// 		log.Fatal("error closing database")
-	// 	}
-	// }()
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal("error closing database")
+		}
+	}()
 
 	pingErr := db.Ping()
 	if pingErr != nil {
 		log.Fatalf("can't ping db:\n%s", err)
 	}
 
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	/** API **/
+	// gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
 
-	if err != nil {
-		log.Fatalf("error configuring driver:\n%s", err)
-	}
+	router.GET("/test", func(c *gin.Context) {
+		c.String(http.StatusOK, "test GET message")
+	})
 
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://./migrations",
-		"postgres", driver)
-
-	if err != nil {
-		log.Fatalf("error migrating:\n%s", err)
-	}
-
-	err = m.Up()
-
-	if err != nil {
-		log.Fatalf("error migrating db:\n%s", err)
-	}
-
-	// router := gin.Default()
-	//
-	// router.GET("/test", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "test GET message")
-	// })
-	//
-	// router.Run(":8080")
+	router.Run(":8080")
 }
