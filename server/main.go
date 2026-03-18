4493dd0bb800b05b6e385830c0e1f4d7a7b48433
Linh Vo <lhvo2112@gmail.com>
2026-03-18 12:38:16 +0200

    server: add database schema and migrations (#1)
    * add database schema and migration
    
    * move database migration to docker container
    
    * tidy go modules

--- server/main.go
+++ server/main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	/** DATABASE **/
+	db_url := os.Getenv("APP_POSTGRES_URL")
+
+	db, err := sql.Open("postgres", db_url)
+
+	if err != nil {
+		log.Fatalf("error connecting to database:\n%s", err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal("error closing database")
+		}
+	}()
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatalf("can't ping db:\n%s", err)
+	}
+
+	/** API **/
+	// gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
 	router.GET("/test", func(c *gin.Context) {
