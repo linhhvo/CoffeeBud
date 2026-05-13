3453124a58f4c560579742ccc8d1bf574d472679
Linh Vo <lhvo2112@gmail.com>
2026-05-13 21:34:46 +0300

    fix deploy

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -8,15 +8,16 @@ import (
 	"coffee-bud/internal/websocket"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load("./.env", "../.env"); err != nil {
-		log.Fatalf("error loading environments: %v", err.Error())
-	}
+	// if err := godotenv.Load("./.env", "../.env"); err != nil {
+	// 	log.Fatalf("error loading environments: %v", err.Error())
+	// }
 
 	session.Init()
 
@@ -36,18 +37,18 @@ func main() {
 	// gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	// router.Use(
-	// 	cors.New(
-	// 		cors.Config{
-	// 			AllowOrigins:     []string{"https://coffeebud-client.fly.dev"},
-	// 			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-	// 			AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-	// 			ExposeHeaders:    []string{"Content-Length"},
-	// 			AllowCredentials: true,
-	// 			MaxAge:           12 * time.Hour,
-	// 		},
-	// 	),
-	// )
+	router.Use(
+		cors.New(
+			cors.Config{
+				AllowOrigins:     []string{"https://coffeebud-client.fly.dev"},
+				AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+				AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+				ExposeHeaders:    []string{"Content-Length"},
+				AllowCredentials: true,
+				MaxAge:           12 * time.Hour,
+			},
+		),
+	)
 
 	router.Use(middleware.ErrorHandler())
 
